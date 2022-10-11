dcfe7aaf80834718f5c55626c1aa3f99fe60ddc6
Isaias R. Lopez <isaias.ramos.27@unsch.edu.pe>
2022-10-11 11:22:32 -0500

    Homework compleatdo.

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,10 +1,26 @@
-'use strict';
+"use strict";
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+  let decimal = 0;
+  let potencia = 0;
+  while (num > 0) {
+    decimal += (num % 10) * Math.pow(2, potencia);
+    num = Math.floor(num / 10);
+    potencia++;
+  }
+  return decimal;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+  let array = [];
+  while (num > 0) {
+    array.push(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return array.reverse().join("").toString();
+}
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
+  BinarioADecimal,
+  DecimalABinario,
 };
