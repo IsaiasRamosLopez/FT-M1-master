2ab539245ed3829126aca099bf140197bcdd3df1
Isaias R. Lopez <isaias.ramos.27@unsch.edu.pe>
2022-10-18 21:06:11 -0500

    (---) => {---}

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  Implementar la clase BinarySearchTree, definiendo los siguientes métodos recursivos:
@@ -9,11 +9,96 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree() {}
+// function BinarySearchTree() {}
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  size() {
+    let count = 1;
+    if (this.left !== null) {
+      count += this.left.size();
+    }
+    if (this.right !== null) {
+      count += this.right.size();
+    }
+    return count;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+
+  contains(value) {
+    if (value === this.value) {
+      return true;
+    } else if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    }
+  }
+
+  depthFirstForEach(fn, order) {
+    if (order === "pre-order") {
+      fn(this.value);
+    }
+    if (this.left !== null) {
+      this.left.depthFirstForEach(fn, order);
+    }
+    if (order === "in-order" || order === undefined) {
+      fn(this.value);
+    }
+    if (this.right !== null) {
+      this.right.depthFirstForEach(fn, order);
+    }
+    if (order === "post-order") {
+      fn(this.value);
+    }
+  }
+
+  breadthFirstForEach(fn) {
+    let queue = [];
+    queue.push(this);
+    while (queue.length > 0) {
+      let node = queue.shift();
+      fn(node.value);
+      if (node.left !== null) {
+        queue.push(node.left);
+      }
+      if (node.right !== null) {
+        queue.push(node.right);
+      }
+    }
+  }
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
-   BinarySearchTree,
+  BinarySearchTree,
 };
